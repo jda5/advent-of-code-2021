4c8752f4e5392fb674c48cf32279a3498d0d5d25
jda5 <74459860+jda5@users.noreply.github.com>
2021-12-01 16:34:10 +0000

    Speed optimisations: solution_two now O(n)

--- 01-sonar-sweep.py
+++ 01-sonar-sweep.py
@@ -19,10 +19,9 @@ def solution_one():
 
 def solution_two():
     res = 0
-    prev_window = sum(puzzle_input[:3])
+    prev_window = puzzle_input[0]
     for i in range(2, len(puzzle_input) - 1):
-        window = sum([puzzle_input[i-1], puzzle_input[i], puzzle_input[i+1]])
-        if window > prev_window:
+        if puzzle_input[i+1] > prev_window:
             res += 1
-        prev_window = window
+        prev_window = puzzle_input[i-1]
     return res
