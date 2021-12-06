9c39ba97d0c60db5b7175da9298ba1c5c31cecd2
jda5 <74459860+jda5@users.noreply.github.com>
2021-12-06 13:09:38 +0000

    Add files via upload

---
+++ 06-lanternfish.py
@@ -0,0 +1,29 @@
+import os
+
+FILENAME = os.path.splitext(__file__)
+PUZZLE_INPUT = os.path.join('.', 'puzzle-inputs', f'{FILENAME[0]}.txt')
+
+with open(PUZZLE_INPUT, 'r') as f:
+    puzzle_input = f.read().split(',')
+
+fishes = list(map(int, puzzle_input))
+
+
+def count_fish(days):
+
+    counter = [0 for _ in range(9)]
+
+    for fish in fishes:
+        counter[fish] += 1
+        
+    for _ in range(days):
+        prev = 0
+        for i in range(len(counter) - 1, -1, -1):
+            temp = counter[i]
+            counter[i] = prev
+            prev = temp
+
+        counter[-1] = prev
+        counter[6] += prev
+
+    return sum(counter)
