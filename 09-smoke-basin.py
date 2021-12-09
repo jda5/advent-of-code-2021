9b5638f4cf3d19a2c684d80d64a853ef9a657c84
jda5 <74459860+jda5@users.noreply.github.com>
2021-12-09 09:11:50 +0000

    Cleaning up code

--- 09-smoke-basin.py
+++ 09-smoke-basin.py
@@ -14,9 +14,6 @@ with open(PUZZLE_INPUT, 'r') as f:
         height_map.append(r)
 
 
-# x is col index
-# y is row index
-
 class HeightMap:
 
     def __init__(self, _map):
@@ -30,12 +27,16 @@ class HeightMap:
             for x in range(len(self._map[0])):
                 yield y, x
 
-    def is_low_point(self, x, y):
-        point = self._map[y][x]
+    def get_adjacent(self, x, y):
         for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
             if self.on_map(x + dx, y + dy):
-                if self._map[y + dy][x + dx] <= point:
-                    return False
+                yield y + dy, x + dx
+
+    def is_low_point(self, x, y):
+        point = self._map[y][x]
+        for dy, dx in self.get_adjacent(x, y):
+            if self._map[dy][dx] <= point:
+                return False
         return True
 
     def solution_one(self):
@@ -51,29 +52,18 @@ class HeightMap:
         while len(points) > 0:
             y, x = points.pop()
             visited.add((y, x))
-            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                if self.on_map(x + dx, y + dy):
-                    if self._map[y + dy][x + dx] < 9:
-                        if ((y + dy), (x + dx)) not in visited:
-                            points.append((y + dy, x + dx))
+            for dy, dx in self.get_adjacent(x, y):
+                if self._map[dy][dx] < 9:
+                    if ((dy), (dx)) not in visited:
+                        points.append((dy, dx))
         return len(visited)
 
     def solution_two(self):
-        low_points = []
+        top_three = [0, 0, 0]
         for y, x in self.get_points():
             if self.is_low_point(x, y):
-                low_points.append((y, x))
-
-        sizes = []
-        for y, x in low_points:
-            sizes.append(self.traverse_basin(x, y))
-
-        top_three = []
-        for i in range(0, 3): 
-            curr = 0
-            for j in range(len(sizes)):     
-                if sizes[j] > curr:
-                    curr = sizes[j];
-            sizes.remove(curr);
-            top_three.append(curr)
+                basin_size = self.traverse_basin(x, y)
+                index_min = min(range(len(top_three)), key=top_three.__getitem__)
+                if basin_size > top_three[index_min]:
+                    top_three[index_min] = basin_size
         return prod(top_three)
